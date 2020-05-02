4bcfbd4f8873c256fc434e322d2831e96a3eb185
aziz <19atabaev.aziz@gmail.com>
2020-05-02 14:38:35 +0500

    minutes, seconds, milliseconds are installed and fixed 2.0 version

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -8,18 +8,25 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     
-    private  int  milliseconds;
+    private  long  milliseconds;
     private boolean running;
-    
+    long startmillis;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         runTimer();
     }
 
     public void start(View view) {
+        if (startmillis == 0) {
+          startmillis = System.currentTimeMillis();
+        } else {
+          startmillis = System.currentTimeMillis() - milliseconds;
+        }
         running = true;
     }
 
@@ -38,14 +45,14 @@ public class MainActivity extends AppCompatActivity {
         handler.post(new Runnable() {
             @Override
             public void run() {
-                int minute = milliseconds/6000;
-                int second = (milliseconds%6000)/100;
-                int millisecond = milliseconds%100;
+                long minute = milliseconds/60000;
+                long second = (milliseconds%60000)/1000;
+                long ms = milliseconds%1000;
 
-                String time = String.format("%d:%02d:%02d", minute, second, millisecond);
+                String time = String.format("%d:%02d.%02d", minute, second, ms/10);
                 textView.setText(time);
                 if(running){
-                    milliseconds++;
+                    milliseconds = System.currentTimeMillis() - startmillis;
                 }
                 handler.postDelayed(this, 10);
             }
